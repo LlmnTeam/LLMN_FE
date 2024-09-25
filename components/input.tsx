95447fe9f25984da147b8c7097ec11193e706af2
leejongil1046 <leejongil1046@gmail.com>
2024-09-26 03:04:17 +0900

    feat: Add download icon in signup-step3 page

--- components/input.tsx
+++ components/input.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface InputProps {
   type: string;
   label: string;
@@ -27,6 +29,18 @@ export default function Input({
         placeholder={placeholder}
         className="appearance-none w-full h-[50px] xs:h-[60px] text-[14px] xs:text-[16px] text-gray-600 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:ring-offset-0 focus:ring-1"
       />
+      {label === "프라이빗 키" ? (
+        <div className="flex flex-row justify-center items-center absolute top-3 right-4 cursor-pointer">
+          <Image
+            src="/images/download_icon.svg"
+            alt="download_icon"
+            width={36}
+            height={33}
+            className="xs:w-[36px] xs:h-[33px] w-[26px] h-[25px]"
+          />
+          <input type="file" className="hidden" />
+        </div>
+      ) : null}
     </div>
   );
 }
