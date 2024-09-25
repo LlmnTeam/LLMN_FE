1e661f33545d0cbd5f8244e179274935f6250cb6
leejongil1046 <leejongil1046@gmail.com>
2024-09-25 19:03:44 +0900

    style: Unify logo position in signup form

--- pages/login/signup-step2.tsx
+++ pages/login/signup-step2.tsx
@@ -1,11 +1,10 @@
-import Button from "@/components/button";
 import Input from "@/components/input";
 import Image from "next/image";
 
 export default function SignupStep2() {
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen gap-7 px-6 pb-[15vh] overflow-hidden">
-      <div className="flex flex-row justify-center items-center gap-3 mb-4">
+    <div className="flex flex-col justify-start items-center w-screen h-screen gap-7 px-6 pt-[15vh] overflow-hidden">
+      <div className="flex flex-row justify-center items-center gap-3">
         <Image src="/images/logo.svg" alt="logo" width={46} height={54} />
         <span className="text-3xl font-semibold">LLMN</span>
       </div>
@@ -19,7 +18,7 @@ export default function SignupStep2() {
         placeholder="비밀번호를 입력해주세요."
       />
       <div className="w-full max-w-[605px] px-1 -mt-6 text-[13px] xs:text-[15px] font-semibold text-red-500">
-        이미 존재하는 이메일입니다.
+        비밀번호가 유효하지 않습니다.
       </div>
       <Input
         type="password"
