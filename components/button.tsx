daa6f5017d11ca171c0359a2da2aed0f10fba6f3
leejongil1046 <leejongil1046@gmail.com>
2024-09-25 17:02:33 +0900

    feat: Add signup-step1 page with responsive design and Link tag in button component

--- components/button.tsx
+++ components/button.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface ButtonProps {
   label: string;
   kind: string;
@@ -6,19 +8,24 @@ interface ButtonProps {
 
 export default function Button({
   label,
+  marginTop,
   kind = "login",
   ...rest
 }: ButtonProps) {
   return (
-    <div className="flex flex-row justify-center items-center relative w-full max-w-[605px] mt-4 xs:mt-7">
+    <div
+      className={`flex flex-row justify-center items-center relative w-full max-w-[605px] mt-3 xs:mt-5`}
+    >
       <button className="w-full h-[52px] xs:h-[64px] rounded-md bg-[#0F172A] text-white text-[18px] xs:text-[22px] font-semibold">
         {label}
       </button>
       {kind === "login" ? (
         <>
-          <span className="absolute left-0.5 top-[55px] xs:top-[70px] text-[14px] xs:text-[16px] font-semibold text-[#717478] cursor-pointer">
-            회원가입
-          </span>
+          <Link href="/login/signup-step1">
+            <span className="absolute left-0.5 top-[55px] xs:top-[70px] text-[14px] xs:text-[16px] font-semibold text-[#717478] cursor-pointer">
+              회원가입
+            </span>
+          </Link>
           <span className="absolute right-0.5 top-[55px] xs:top-[70px] text-[14px] xs:text-[16px] font-semibold text-[#717478] cursor-pointer">
             비밀번호 찾기
           </span>
---
+++ pages/login/signup-step1.tsx
@@ -0,0 +1,28 @@
+import Button from "@/components/button";
+import Input from "@/components/input";
+import Image from "next/image";
+
+export default function SignupStep1() {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen gap-3 px-6 pb-[15vh] overflow-hidden">
+      <div className="flex flex-row justify-center items-center gap-3 mb-4">
+        <Image src="/images/logo.svg" alt="logo" width={46} height={54} />
+        <span className="text-3xl font-semibold">LLMN</span>
+      </div>
+      <Input name="email" label="이메일" />
+      {/* <div className="w-full max-w-[605px] -mt-2 text-[13px] xs:text-[15px] font-semibold text-red-500">
+        이미 존재하는 이메일입니다.
+      </div> */}
+      <div className="flex flex-row justify-end items-center w-full max-w-[605px] -mt-2 text-[14px] xs:text-[16px] font-semibold text-[#717478] cursor-pointer">
+        재전송하기
+      </div>
+      <div className="flex flex-row justify-center items-center w-full max-w-[605px] h-[75px] xs:h-[96px] text-[14px] xs:text-[16px] text-center -mb-5 xs:-mb-6 bg-[#F8F9FA] rounded-lg">
+        해당 이메일로 인증코드를 전송하였습니다.
+        <br />
+        아래에 인증코드를 입력해주세요.
+      </div>
+      <Input name="code" label="" />
+      <Button label="중복확인" kind="check" />
+    </div>
+  );
+}
