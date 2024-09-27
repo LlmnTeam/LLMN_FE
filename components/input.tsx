545d6c2f7ebc437b33086d852ec6986e3067fade
leejongil1046 <leejongil1046@gmail.com>
2024-09-27 12:59:30 +0900

    feat: Add new-item page with responsive desgin and modify styles of Input component

--- components/input.tsx
+++ components/input.tsx
@@ -4,6 +4,7 @@ interface InputProps {
   type: string;
   label: string;
   placeholder: string;
+  maxWidth?: string;
   [key: string]: any;
 }
 
@@ -11,14 +12,18 @@ export default function Input({
   type,
   label,
   placeholder,
+  maxWidth = "605px",
   ...rest
 }: InputProps) {
   return (
-    <div className="flex flex-col justify-center items-center relative w-full max-w-[605px] mt-5 xs:mt-8">
+    <div
+      className="flex flex-col justify-center items-center relative w-full mt-5 xs:mt-8"
+      style={{ maxWidth: maxWidth }}
+    >
       {label?.trim() ? (
         <label
           htmlFor={type}
-          className="absolute -top-6 xs:-top-7 left-1 text-[15px] xs:text-[18px]"
+          className="absolute -top-6 xs:-top-7 left-1 text-[17px] xs:text-[18px] sm:text-[19px]"
         >
           {label}
         </label>
@@ -27,7 +32,7 @@ export default function Input({
         id={type}
         type={type}
         placeholder={placeholder}
-        className="appearance-none w-full h-[50px] xs:h-[60px] text-[14px] xs:text-[16px] text-gray-600 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:ring-offset-0 focus:ring-1"
+        className="appearance-none w-full h-[44px] xs:h-[52px] sm:h-[60px] text-[14px] xs:text-[15px] sm:text-[16px] text-gray-600 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:ring-offset-0 focus:ring-1"
       />
       {label === "프라이빗 키" ? (
         <div className="flex flex-row justify-center items-center absolute top-3 right-4 cursor-pointer">
--- pages/new-item.tsx
+++ pages/new-item.tsx
@@ -1,10 +1,48 @@
+import Input from "@/components/input";
 import Layout from "@/components/layout";
 import Image from "next/image";
 
 export default function NewItem() {
   return (
     <Layout>
-      <div className="px-5 xs:px-7 sm:px-10"></div>
+      <div className="px-5 xs:px-7 sm:px-10">
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-col justify-start items-start gap-1 xs:gap-2">
+            <span className="text-[24px] xs:text-[30px] sm:text-[36px] text-black font-bold pl-1">
+              새로운 아이템
+            </span>
+            <span className="text-[12px] xs:text-[15px] sm:text-[18px] text-[#979797] font-semibold">
+              {"<주의>"} 컨테이너의 이름을 추후에 변경하면 기능이 정상적으로
+              작동하지 않을 수 있습니다.
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col justify-start items-start gap-5 xs:gap-7 sm:gap-8 mt-3 xs:mt-4 sm:mt-5">
+          <Input
+            type="text"
+            label="프로젝트 이름"
+            placeholder="이름을 입력해주세요."
+            maxWidth="1000px"
+          />
+          <Input
+            type="text"
+            label="설명"
+            placeholder="설명을 입력해주세요."
+            maxWidth="1000px"
+          />
+          <Input
+            type="email"
+            label="컨테이너 이름"
+            placeholder="연결할 컨테이너를 선택해주세요."
+            maxWidth="1000px"
+          />
+          <div className="flex flex-row justify-end items-center w-full max-w-[1000px] mt-40 xs:mt-44 sm:mt-48">
+            <button className="w-[64px] xs:w-[80px] h-[40px] xs:h-[50px] text-[16px] xs:text-[20px] text-white bg-[#0F172A] rounded-md">
+              생성
+            </button>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
