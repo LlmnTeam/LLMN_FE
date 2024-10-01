2d9a434567cc2c3bab2a1e87fb0d90facd46438a
leejongil1046 <leejongil1046@gmail.com>
2024-10-01 23:31:27 +0900

    feat: Add ConfirmModal component

---
+++ components/confirm-modal.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  message: string;
+  buttonText: string;
+}
+
+export default function LogFileModal({
+  isOpen,
+  onClose,
+  title,
+  message,
+  buttonText,
+}: ModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div
+        className="fixed inset-0 bg-black opacity-70"
+        onClick={onClose}
+      ></div>
+      <div className="w-[90%] xs:w-[80%] sm:w-[548px] bg-white px-6 xs:px-8 sm:px-10 py-4 xs:py-5 sm:py-6 rounded-xl shadow-lg z-10">
+        <div className="flex flex-row justify-between items-center">
+          <div className="text-[18px] xs:text-[20px] sm:text-[22px] font-bold ml-1">
+            {title}
+          </div>
+          <div
+            className="flex flex-row justify-center items-center w-[24px] xs:w-[27px] sm:w-[30px] h-[24px] xs:h-[27px] sm:h-[30px] rounded-full bg-[#E5E5E5] text-[12px] xs:text-[14px] sm:text-[16px] mr-1"
+            onClick={onClose}
+          >
+            ✕
+          </div>
+        </div>
+        <div>{message}</div>
+        <div className="flex flex-row justify-center items-center w-full mt-3 xs:mt-4 sm:mt-5">
+          <button
+            onClick={onClose}
+            className="w-[84px] xs:w-[90px] sm:w-[96px] h-[34px] xs:h-[36px] sm:h-[39px] text-[14px] xs:text-[15px] sm:text-[16px] text-white font-semibold bg-[#0F172A] rounded-md"
+          >
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
