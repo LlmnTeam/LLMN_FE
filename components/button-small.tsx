2f0dd5bc96c45e85fd19137d155561ea05d6f88c
leejongil1046 <leejongil1046@gmail.com>
2024-10-05 22:32:00 +0900

    style: Reduce height of ButtonSmall component

--- components/button-small.tsx
+++ components/button-small.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 export default function ButtonSmall({ label, ...rest }: ButtonProps) {
   return (
     <div className="flex flex-row justify-center items-center relative">
-      <button className="h-[45px] xs:h-[50px] sm:h-[55px] text-[16px] xs:text-[18px] sm:text-[20px] rounded-md bg-[#0F172A] text-white font-semibold px-[20px] xs:px-[22px] sm:px-[24px]">
+      <button className="h-[40px] xs:h-[45px] sm:h-[50px] text-[16px] xs:text-[18px] sm:text-[20px] rounded-md bg-[#0F172A] text-white font-semibold px-[20px] xs:px-[22px] sm:px-[24px]">
         {label}
       </button>
     </div>
