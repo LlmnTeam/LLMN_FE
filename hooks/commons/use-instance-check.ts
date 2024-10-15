cbe2971cb22e7381c89ef1e1c18533a7e7e9987a
leejongil1046 <leejongil1046@gmail.com>
2024-10-15 19:00:58 +0900

    feat: Add remoteHostMsg and apply it in signup-step3 page

--- hooks/commons/use-instance-check.ts
+++ hooks/commons/use-instance-check.ts
@@ -1,9 +1,31 @@
 import { useState, useEffect } from "react";
+import { useIsMobile } from "./use-is-mobile";
+
+const validateIPv4 = (value: string) => {
+  const pattern =
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  return pattern.test(value);
+};
+
+const validateIPv6 = (value: string) => {
+  const pattern = /^([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4})$/;
+  return pattern.test(value);
+};
+
+const filterInput = (value: string, isIPv6: boolean): string => {
+  if (isIPv6) {
+    return value.replace(/[^0-9a-fA-F:]/g, "");
+  } else {
+    return value.replace(/[^0-9.]/g, "");
+  }
+};
 
 interface UseInstanceCheckReturn {
   remoteName: string;
   remoteHost: string;
   remoteKeyPath: string;
+  remoteHostMsg: string;
+  isValidRemoteHost: boolean | null;
   handleRemoteNameChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   handleRemoteHostChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   handleRemoteKeyPathChange: (
@@ -15,13 +37,18 @@ export const useInstanceCheck = (): UseInstanceCheckReturn => {
   const [remoteName, setRemoteName] = useState<string>("");
   const [remoteHost, setRemoteHost] = useState<string>("");
   const [remoteKeyPath, setRemoteKeyPath] = useState<string>("");
-  const [isPasswordValid, setIsPasswordValid] = useState<boolean | null>(null);
-  const [isPasswordMatching, setIsPasswordMatching] = useState<boolean | null>(
+  const [remoteHostMsg, setRemoteHostMsg] = useState<string>(
+    "IPv4 또는 IPv6 형식 중 하나만 입력하세요."
+  );
+  const [isValidRemoteHost, setIsValidRemoteHost] = useState<boolean | null>(
     null
   );
-  const [validationMessage, setValidationMessage] =
-    useState<string>("비밀번호를 입력해주세요.");
-  const [confirmMessage, setConfirmMessage] = useState<string>("");
+
+  const isMobile = useIsMobile(640);
+
+  useEffect(() => {
+    console.log("isMobile: ", isMobile);
+  }, [isMobile]);
 
   const handleRemoteNameChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -32,8 +59,49 @@ export const useInstanceCheck = (): UseInstanceCheckReturn => {
   const handleRemoteHostChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ): void => {
-    setRemoteHost(event.target.value);
+    let value = event.target.value;
+
+    const isIPv6 = value.includes(":");
+
+    value = filterInput(value, isIPv6);
+
+    if (isIPv6) {
+      if (validateIPv6(value)) {
+        setIsValidRemoteHost(true);
+        setRemoteHostMsg("유효한 IPv6 주소입니다.");
+      } else {
+        setIsValidRemoteHost(false);
+        setRemoteHostMsg(
+          isMobile
+            ? "IPv6 예: 2001:db8::1"
+            : "IPv6 형식에 맞춰 입력하세요. 예: 2001:0db8:85a3:0000:0000:8a2e:0370:7334"
+        );
+      }
+    } else {
+      if (validateIPv4(value)) {
+        setIsValidRemoteHost(true);
+        setRemoteHostMsg("유효한 IPv4 주소입니다.");
+      } else {
+        setIsValidRemoteHost(false);
+        setRemoteHostMsg("IPv4 형식에 맞춰 입력하세요. 예: 192.168.0.1");
+      }
+    }
+
+    setRemoteHost(value);
   };
+
+  useEffect(() => {
+    if (remoteHost.includes(":")) {
+      setRemoteHostMsg(
+        isMobile
+          ? "IPv6 형식에 맞춰 입력하세요. 예: 2001:db8::1"
+          : "IPv6 형식에 맞춰 입력하세요. 예: 2001:0db8:85a3:0000:0000:8a2e:0370:7334"
+      );
+    }
+
+    console.log("isMobile: ", isMobile);
+    console.log("remoteHostMsg: ", remoteHostMsg);
+  }, [isMobile, isValidRemoteHost, remoteHost]);
 
   const handleRemoteKeyPathChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -45,6 +113,8 @@ export const useInstanceCheck = (): UseInstanceCheckReturn => {
     remoteName,
     remoteHost,
     remoteKeyPath,
+    remoteHostMsg,
+    isValidRemoteHost,
     handleRemoteNameChange,
     handleRemoteHostChange,
     handleRemoteKeyPathChange,
--- pages/login/signup-step3.tsx
+++ pages/login/signup-step3.tsx
@@ -1,10 +1,22 @@
 import ButtonSmall from "@/components/commons/button-small";
 import Input from "@/components/commons/input";
 import Logo from "@/components/commons/logo";
+import { useInstanceCheck } from "@/hooks/commons/use-instance-check";
 import { cls } from "@/libs/utils";
 import { useState } from "react";
 
 export default function SignupStep3() {
+  const {
+    remoteName,
+    remoteHost,
+    remoteKeyPath,
+    remoteHostMsg,
+    isValidRemoteHost,
+    handleRemoteNameChange,
+    handleRemoteHostChange,
+    handleRemoteKeyPathChange,
+  } = useInstanceCheck();
+
   return (
     <div>
       <div className="flex flex-col justify-start items-center w-screen max-w-[605px] mx-auto h-[690px] gap-8 xs:gap-9 sm:gap-10 px-6 pt-[15vh] overflow-y-auto overflow-x-hidden">
@@ -14,6 +26,8 @@ export default function SignupStep3() {
             type="text"
             label="원격 서버 사용자"
             placeholder="원격 서버의 사용자명을 입력해주세요."
+            value={remoteName}
+            onChange={handleRemoteNameChange}
           />
           <div
             className={cls(
@@ -28,13 +42,20 @@ export default function SignupStep3() {
             type="text"
             label="호스트"
             placeholder="원격 서버의 IP 주소를 입력해주세요."
+            value={remoteHost}
+            onChange={handleRemoteHostChange}
           />
           <div
             className={cls(
-              "w-full max-w-[605px] absolute top-[45px] xs:top-[50px] sm:top-[55px] text-[12px] xs:text-[13px] sm:text-[14px] font-semibold px-1 mt-0.5 text-gray-400"
+              "w-full max-w-[605px] absolute top-[45px] xs:top-[50px] sm:top-[55px] text-[12px] xs:text-[13px] sm:text-[14px] font-semibold px-1 mt-0.5",
+              isValidRemoteHost === null
+                ? "text-gray-400"
+                : isValidRemoteHost
+                ? "text-blue-400"
+                : "text-red-400"
             )}
           >
-            내용을 입력하세요.
+            {remoteHostMsg}
           </div>
         </div>
         <div className="flex flex-col justify-start items-center relative w-full mt-7 xs:mt-8 sm:mt-9">
@@ -42,14 +63,17 @@ export default function SignupStep3() {
             type="text"
             label="프라이빗 키"
             placeholder="SSH perm 키를 업로드해주세요."
+            value={remoteKeyPath}
+            onChange={handleRemoteKeyPathChange}
+            readOnly
           />
-          <div
+          {/* <div
             className={cls(
               "w-full max-w-[605px] absolute top-[45px] xs:top-[50px] sm:top-[55px] text-[12px] xs:text-[13px] sm:text-[14px] font-semibold px-1 mt-0.5 text-gray-400"
             )}
           >
             내용을 입력하세요.
-          </div>
+          </div> */}
         </div>
       </div>
       <div className="flex flex-row justify-end items-center gap-1 xs:gap-2 sm:gap-3 w-full max-w-[605px] mx-auto px-6">
