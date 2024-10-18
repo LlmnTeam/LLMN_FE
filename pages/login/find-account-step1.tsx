12e84d390a3dcca853d209bf71b70c358ffe9b21
leejongil1046 <leejongil1046@gmail.com>
2024-10-18 16:57:40 +0900

    feat: Update find-account pages with updated code

--- pages/login/find-account-step1.tsx
+++ pages/login/find-account-step1.tsx
@@ -1,29 +1,178 @@
-import Button from "@/components/commons/button-large";
+import ButtonLarge from "@/components/commons/button-large";
 import Input from "@/components/commons/input";
-import Image from "next/image";
+import Logo from "@/components/commons/logo";
+import useEmailCheck from "@/hooks/login/use-email-check";
+import { cls } from "@/libs/class-utils";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function FindAccountStep1() {
+  const router = useRouter();
+
+  const {
+    email,
+    code,
+    isValidEmail,
+    isValidCode,
+    emailMsg,
+    codeMsg,
+    timer,
+    handleEmailChange,
+    handleCodeChange,
+    verifyEmail,
+    verifyCode,
+    resendCode,
+  } = useEmailCheck();
+
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    setDisabled(isValidEmail && isValidCode ? false : true);
+  }, [isValidEmail, isValidCode]);
+
+  const handleNextButton = (): void => {
+    if (!isValidEmail || !isValidCode) return;
+    sessionStorage.setItem("email", email);
+    router.push("/login/signup-step2");
+  };
+
   return (
-    <div className="flex flex-col justify-start items-center w-screen h-screen gap-3 px-6 pt-[15vh] overflow-hidden">
-      <div className="flex flex-row justify-center items-center gap-3 mb-4">
-        <Image src="/images/logo.svg" alt="logo" width={46} height={54} />
-        <span className="text-3xl font-semibold">LLMN</span>
+    <div className="flex flex-col justify-start items-center w-screen max-w-[605px] mx-auto h-screen gap-8 xs:gap-9 sm:gap-10 pt-[15vh] pb-[15vh] px-6 overflow-y-scroll overflow-x-hidden scrollbar-hide">
+      <Logo />
+      <div className="flex flex-col justify-start items-center relative w-full mt-8 xs:mt-9 sm:mt-10">
+        <Input
+          type="email"
+          label="이메일"
+          placeholder="이메일을 입력해주세요."
+          value={email}
+          onChange={handleEmailChange}
+          readOnly={isValidEmail ? true : false}
+        />
+        <div className="flex flex-row justify-between items-center absolute top-[45px] xs:top-[50px] sm:top-[55px] w-full">
+          <div
+            className={cls(
+              "text-[12px] xs:text-[13px] sm:text-[14px] font-semibold",
+              isValidEmail
+                ? "visible text-blue-400"
+                : !isValidEmail && email.trim()
+                ? "visible text-red-400"
+                : "hidden"
+            )}
+          >
+            {emailMsg}
+          </div>
+          <div
+            className={cls(
+              "text-[14px] xs:text-[15px] sm:text-[16px] font-semibold transition-colors",
+              isValidEmail ? "visible" : "hidden",
+              timer === 0
+                ? "text-[#717478] cursor-pointer"
+                : "text-gray-300 cursor-not-allowed"
+            )}
+            onClick={() => resendCode()}
+          >
+            재전송하기
+          </div>
+        </div>
       </div>
-      <Input type="email" label="이메일" placeholder="이메일을 입력해주세요." />
-      <div className="w-full max-w-[605px] px-1 -mt-2 text-[13px] xs:text-[15px] font-semibold text-red-500">
-        이미 존재하는 이메일입니다.
-      </div>
-      <Button label="중복확인" kind="check" />
-      {/* <div className="flex flex-row justify-end items-center w-full max-w-[605px] -mt-2 text-[14px] xs:text-[16px] font-semibold text-[#717478] cursor-pointer">
-        재전송하기
-      </div>
-      <div className="flex flex-row justify-center items-center w-full max-w-[605px] h-[75px] xs:h-[96px] text-[14px] xs:text-[16px] text-center -mb-5 xs:-mb-6 bg-[#F8F9FA] rounded-lg">
-        해당 이메일로 인증코드를 전송하였습니다.
-        <br />
-        아래에 인증코드를 입력해주세요.
-      </div>
-      <Input type="code" label="" placeholder="인증 코드를 입력해주세요." />
-      <Button label="인증하기" kind="check" /> */}
+      {/* {!isValidEmail ? (
+        <ButtonLarge
+          label="중복확인"
+          kind="check"
+          onClick={() => verifyEmail()}
+        />
+      ) : (
+        <>
+          <div className="flex flex-row justify-center items-center w-full text-[13px] xs:text-[14px] sm:text-[15px] text-center py-3 xs:py-4 sm:py-5 mt-1 xs:mt-2 sm:mt-3 -mb-3 xs:-mb-4 sm:-mb-5 bg-[#F8F9FA] rounded-lg">
+            해당 이메일로 인증코드를 전송하였습니다.
+            <br />
+            아래에 인증코드를 입력해주세요.
+          </div>
+          <div className="flex flex-col justify-start items-center relative w-full mt-2.5 xs:mt-3 sm:mt-3.5 mb-2 xs:mb-3 sm:mb-4">
+            <Input
+              type="code"
+              label=""
+              placeholder="인증 코드를 입력해주세요."
+              value={code}
+              onChange={handleCodeChange}
+            />
+            <div
+              className={cls(
+                "flex flex-row justify-center items-center h-full absolute right-4 text-[14px] xs:text-[15px] sm:text-[16px] font-medium text-gray-600",
+                isValidEmail ? "visible" : "hidden"
+              )}
+            >
+              <div className="w-[40px]">
+                {Math.floor(timer / 60)}:
+                {(timer % 60).toString().padStart(2, "0")}
+              </div>
+            </div>
+            <div
+              className={cls(
+                "absolute top-[45px] xs:top-[50px] sm:top-[55px] w-full text-[12px] xs:text-[13px] sm:text-[14px] font-semibold px-1 mt-0.5",
+                codeMsg === ""
+                  ? "hidden"
+                  : isValidCode
+                  ? "visible text-blue-400"
+                  : "visible text-red-400"
+              )}
+            >
+              {codeMsg}
+            </div>
+          </div>
+          <ButtonLarge
+            label="다음"
+            kind="check"
+            disabled={disabled}
+            onClick={handleNextButton}
+          />
+        </>
+      )} */}
+      <>
+        <div className="flex flex-row justify-center items-center w-full text-[13px] xs:text-[14px] sm:text-[15px] text-center py-3 xs:py-4 sm:py-5 mt-1 xs:mt-2 sm:mt-3 -mb-3 xs:-mb-4 sm:-mb-5 bg-[#F8F9FA] rounded-lg">
+          해당 이메일로 인증코드를 전송하였습니다.
+          <br />
+          아래에 인증코드를 입력해주세요.
+        </div>
+        <div className="flex flex-col justify-start items-center relative w-full mt-2.5 xs:mt-3 sm:mt-3.5 mb-2 xs:mb-3 sm:mb-4">
+          <Input
+            type="code"
+            label=""
+            placeholder="인증 코드를 입력해주세요."
+            value={code}
+            onChange={handleCodeChange}
+          />
+          <div
+            className={cls(
+              "flex flex-row justify-center items-center h-full absolute right-4 text-[14px] xs:text-[15px] sm:text-[16px] font-medium text-gray-600",
+              isValidEmail ? "visible" : "hidden"
+            )}
+          >
+            <div className="w-[40px]">
+              {Math.floor(timer / 60)}:
+              {(timer % 60).toString().padStart(2, "0")}
+            </div>
+          </div>
+          <div
+            className={cls(
+              "absolute top-[45px] xs:top-[50px] sm:top-[55px] w-full text-[12px] xs:text-[13px] sm:text-[14px] font-semibold px-1 mt-0.5",
+              codeMsg === ""
+                ? "hidden"
+                : isValidCode
+                ? "visible text-blue-400"
+                : "visible text-red-400"
+            )}
+          >
+            {codeMsg}
+          </div>
+        </div>
+        <ButtonLarge
+          label="다음"
+          kind="check"
+          disabled={disabled}
+          onClick={handleNextButton}
+        />
+      </>
     </div>
   );
 }
--- pages/login/find-account-step2.tsx
+++ pages/login/find-account-step2.tsx
@@ -1,44 +1,87 @@
+import ButtonSmall from "@/components/commons/button-small";
 import Input from "@/components/commons/input";
-import Image from "next/image";
+import Logo from "@/components/commons/logo";
+import usePasswordCheck from "@/hooks/login/use-password-check";
+import { cls } from "@/libs/class-utils";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-export default function SignupStep2() {
+export default function FindAccountStep2() {
+  const router = useRouter();
+
+  const {
+    password,
+    passwordConfirm,
+    isValidPassword,
+    isPasswordMatching,
+    validationMessage,
+    confirmMessage,
+    handlePasswordChange,
+    handlePasswordConfirmChange,
+  } = usePasswordCheck();
+
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    setDisabled(isValidPassword && isPasswordMatching ? false : true);
+  }, [isValidPassword, isPasswordMatching]);
+
+  const handleNextButton = (): void => {
+    if (disabled) return;
+  };
+
   return (
-    <div className="flex flex-col justify-start items-center w-screen h-screen gap-7 px-6 pt-[15vh] overflow-hidden">
-      <div className="flex flex-row justify-center items-center gap-3">
-        <Image src="/images/logo.svg" alt="logo" width={46} height={54} />
-        <span className="text-3xl font-semibold">LLMN</span>
+    <div>
+      <div className="flex flex-col justify-start items-center w-screen max-w-[605px] mx-auto h-[498px] xs:h-[550px] sm:h-[610px] gap-8 xs:gap-9 sm:gap-10 px-6 pt-[15vh] overflow-y-auto overflow-x-hidden">
+        <Logo />
+        <div className="flex flex-col justify-start items-center relative w-full mt-8 xs:mt-9 sm:mt-10">
+          <Input
+            type="password"
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요."
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <div
+            className={cls(
+              "w-full max-w-[605px] absolute top-[44px] xs:top-[49px] sm:top-[54px] text-[11px] xs:text-[12px] sm:text-[13px] font-semibold px-1 mt-0.5",
+              isValidPassword ? "text-blue-400" : "text-red-400"
+            )}
+          >
+            {validationMessage}
+          </div>
+        </div>
+        <div className="flex flex-col justify-start items-center relative w-full mt-5 xs:mt-6 sm:mt-7">
+          <Input
+            type="password"
+            label="비밀번호 확인"
+            placeholder="비밀번호를 입력해주세요."
+            value={passwordConfirm}
+            onChange={handlePasswordConfirmChange}
+            readOnly={isValidPassword ? false : true}
+          />
+          <div
+            className={cls(
+              "w-full max-w-[605px] absolute top-[44px] xs:top-[49px] sm:top-[54px] text-[11px] xs:text-[12px] sm:text-[13px] font-semibold px-1 mt-0.5",
+              isPasswordMatching ? "text-blue-400" : "text-red-400"
+            )}
+          >
+            {confirmMessage}
+          </div>
+        </div>
       </div>
-      <Input
-        type="password"
-        label="현재 비밀번호"
-        placeholder="비밀번호를 입력해주세요."
-      />
-      <div className="w-full max-w-[605px] px-1 -mt-6 text-[13px] xs:text-[15px] font-semibold text-gray-400">
-        현재 비밀번호를 입력해주세요.
-      </div>
-      <Input
-        type="password"
-        label="새 비밀번호"
-        placeholder="비밀번호를 입력해주세요."
-      />
-      <div className="w-full max-w-[605px] px-1 -mt-6 text-[13px] xs:text-[15px] font-semibold text-red-500">
-        비밀번호가 유효하지 않습니다.
-      </div>
-      <Input
-        type="password"
-        label="새 비밀번호 확인"
-        placeholder="비밀번호를 입력해주세요."
-      />
-      <div className="w-full max-w-[605px] px-1 -mt-6 text-[13px] xs:text-[15px] font-semibold text-red-500">
-        비밀번호가 일치하지 않습니다.
-      </div>
-      <div className="flex flex-row justify-end items-center gap-3 w-full max-w-[605px] text-[20px] font-semibold">
-        <button className="w-[64px] xs:w-[80px] h-[40px] xs:h-[50px] text-[16px] xs:text-[20px] text-black bg-white rounded-md">
+      <div className="flex flex-row justify-end items-center gap-1 xs:gap-2 sm:gap-3 w-full max-w-[605px] mx-auto px-6 pb-[15vh]">
+        <button
+          className="h-[45px] xs:h-[50px] sm:h-[55px] text-[16px] xs:text-[18px] sm:text-[20px] rounded-md bg-white text-black font-semibold px-[20px] xs:px-[22px] sm:px-[24px]"
+          onClick={() => router.push("/login/signup-step1")}
+        >
           취소
         </button>
-        <button className="w-[64px] xs:w-[80px] h-[40px] xs:h-[50px] text-[16px] xs:text-[20px] text-white bg-[#0F172A] rounded-md">
-          다음
-        </button>
+        <ButtonSmall
+          label="다음"
+          disabled={disabled}
+          onClick={handleNextButton}
+        />
       </div>
     </div>
   );
