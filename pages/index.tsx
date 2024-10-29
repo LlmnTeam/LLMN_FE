a6c08834c3d33bf92c4530cf57d2e65592e18de9
leejongil1046 <leejongil1046@gmail.com>
2024-10-30 02:15:43 +0900

    feat: Add blur effect in intro page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import Logo from "@/components/commons/logo";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Index() {
   const router = useRouter();
+  const [isFocused, setIsFocused] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsFocused(true), 500);
+    setTimeout(() => router.push("/login"), 2500);
+  }, [router]);
+
   return (
     <div
-      className="flex flex-col justify-center items-center w-screen h-screen gap-8 xs:gap-9 sm:gap-10 px-6 pb-[15vh] overflow-y-auto overflow-x-hidden"
-      onClick={() => router.push("/login")}
+      className={`flex flex-col justify-center items-center w-screen h-screen overflow-hidden transition-all duration-1000 ${
+        isFocused ? "blur-0" : "blur-md"
+      }`}
     >
       <Logo />
     </div>
